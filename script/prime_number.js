823cbb8064822d9b9de2f816e66506157d978077
Ittisha <annzez@gmail.com>
2016-04-05 18:33:10 +0300

    add script for prime numbers from 2 to 10

---
+++ script/prime_number.js
@@ -0,0 +1,12 @@
+/**
+ * Created by home on 05.04.2016.
+ */
+nextPrime:
+    for (var i = 2; i < 10; i++) {
+
+        for (var j = 2; j < i; j++) {
+            if (i % j == 0) continue nextPrime;
+        }
+
+        alert( i );
+    }
