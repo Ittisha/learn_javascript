e9433a72b904737866284a9a9da3a40d14663295
Ann <annzez@gmail.com>
2016-11-06 15:03:17 +0300

    an example of http-request was added

---
+++ script/request.js
@@ -0,0 +1,28 @@
+function loadPhones() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'script/phones.json', true);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !=4) return;
+        button.parentNode.removeChild(button);
+        if (xhr.status !=200){
+            alert(xhr.status + ': ' + xhr.statusText);
+        } else {
+            try {
+                var phones = JSON.parse(xhr.responseText);
+            } catch (e){
+                alert("Некорректный ответ" + e.message);
+            }
+            showPhones(phones);
+        }
+    }
+    button.innerHTML = "Загружаю...";
+    button.disabled = true;
+}
+function showPhones(phones) {
+    phones.forEach(function(phone){
+        var li = list.appendChild(document.createElement('li'));
+        li.innerHTML = phone.name;
+    });
+
+}
